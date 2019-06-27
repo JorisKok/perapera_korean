58a2f96b4a537369a227880843912808ee694c91
JorisKok <joris_kok1@hotmail.com>
2019-06-27 22:41:44 +0200

    Improve selection and styling

--- index.js
+++ index.js
@@ -1,126 +1,82 @@
-window.addEventListener('mousemove', mouseMove);
-
 // Do not try to translate the last word
 let lastWord;
 
-// Track the word under cursor
-let wordUnderCursor;
+// On select text
+document.onmouseup = document.onkeyup = function (e) {
 
-let pattern = new RegExp(/^[\u3131-\uD79D]+$/ugi);
+    let word = getSelectionText();
 
-function mouseMove(e) {
-    const position = getPosition(e);
-    if (!isText(position)) {
-        updateWordUnderCursor(null);
+    if (!word) {
         return;
     }
 
-    const text = getText(position);
-    const word = getWord(text, getOffset(position));
-    updateWordUnderCursor(word);
-
-    if (!shouldTranslateWord(word)) {
+    if (word === lastWord) {
         return;
     }
+
+    if (!e.clientX || !e.clientY) {
+        return;
+    }
+
     lastWord = word;
 
-    setTimeout(function () {
-        // Only get the translation if the word under cursor is the same as 1 second ago
-        if (word === wordUnderCursor) {
-            getTranslation(word, {left: e.clientX, top: e.clientY});
-        }
-    }, 600)
-}
+    getTranslation(word, {left: e.clientX, top: e.clientY});
+};
 
-function shouldTranslateWord(word) {
-    if (word === null) {
-        return false;
+document.onclick = function (e) {
+    hidePopOver();
+
+    // Reset the last word
+    lastWord = null;
+};
+
+function getSelectionText() {
+    let activeEl = document.activeElement;
+    let activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+    if (activeElTagName === "textarea" || (activeElTagName === "input" && /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) && typeof activeEl.selectionStart == "number") {
+        return activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+    } else if (window.getSelection) {
+        return window.getSelection().toString();
     }
-    if (word === lastWord) {
-        return false;
-    }
-    if (!isKoreanWord(word)) {
-        return false;
-    }
-
-    return true;
-}
-
-function getPosition(e) {
-    if (document.caretPositionFromPoint) {
-        return document.caretPositionFromPoint(e.clientX, e.clientY);
-    }
-    if (document.caretRangeFromPoint) {
-        return document.caretRangeFromPoint(e.clientX, e.clientY);
-    }
-}
-
-function isText(position) {
-    if (position.offsetNode === null) {
-        return false;
-    }
-    return position.offsetNode.nodeName === '#text';
-}
-
-function getText(position) {
-    return position.offsetNode.nodeValue;
-}
-
-function getOffset(position) {
-    return position.offset;
-}
-
-function getWord(text, offset) {
-    const words = text.split(' ');
-    let length = 0;
-    for (let i = 0; i < words.length; i++) {
-        length += words[i].length;
-        if (length >= offset) {
-            return words[i];
-        }
-        length++; // For the spaces between the words that get removed because we split on spaces
-    }
-
     return null;
 }
 
 function getTranslation(word, position) {
-    console.log(word);
     setLoadingText(word, position);
 
     let xhr = new XMLHttpRequest();
     // TODO change after we host it on AWS
-    xhr.open('GET', 'http://0.0.0.0:4001/words?korean=eq.' + word + '&select=korean,word_translations(translation, definition)&word_translations.order=id.asc');
+    xhr.open('GET', 'http://0.0.0.0:4001/words?korean=eq.' + word + '&select=korean,word_translations(related_korean_word,translation, definition)&word_translations.order=id.asc');
+    xhr.setRequestHeader('Range', '1');
     xhr.send(null);
     xhr.onreadystatechange = function () {
         let DONE = 4; // readyState 4 means the request is done.
         let OK = 200; // status 200 is a successful return.
         if (xhr.readyState === DONE) {
             if (xhr.status === OK) {
-                const response = JSON.parse(xhr.responseText);
-                console.log(response);
-                if (response.length === 0) {
-                    hidePopOver();
+                try {
+                    const response = JSON.parse(xhr.responseText);
+                    if (response.length === 0) {
+                        hidePopOver();
 
-                    return;
+                        return;
+                    }
+                    const translation = response[0];
+                    let text = '';
+                    for (let item of translation.word_translations.slice(0, 5)) {
+                        text += '<span style="font-weight: bold; color: #4982de; font-size: 16px; margin-right: 6px;">' + item.related_korean_word + '</span>';
+                        text += '<span style="font-weight: bold;">' + item.translation.replace(';', ',') + '</span>';
+                        text += '<p style="margin-bottom: 1rem;">' + item.definition + '</p>';
+                    }
+                    setText(word, text, position);
+                } catch (error) {
+                    setErrorText(word, position);
                 }
-                const translation = response[0];
-                let text = [];
-                text.push(translation.korean); // TODO do not show this, but the related_korean_word
-                for (let item of translation.word_translations) {
-                    text.push(item.translation); // TODO styling
-                    text.push(item.definition);
-                }
-                setText(word, text.join("\n"), position);
             } else {
                 setErrorText(word, position);
             }
         }
     };
-}
-
-function isKoreanWord(word) {
-    return pattern.test(word);
 }
 
 function getOrCreatePopOver() {
@@ -139,36 +95,45 @@ function getOrCreatePopOver() {
 function hidePopOver() {
     let div = document.getElementById('perapera-window');
     if (div) {
+        div.innerHTML = '';
         return div.style.display = 'none';
     }
 }
 
 function setPopOverPosition(popover, position) {
-    popover.style.left = position.left + 'px';
-    popover.style.top = (position.top + 8) + 'px';
+    popover.style.left = position.left + 10 + 'px';
+    popover.style.top = (position.top + 14 + getScrollHeight()) + 'px';
 
     return popover;
 }
 
 function setText(word, text, position) {
     let popover = setPopOverPosition(getOrCreatePopOver(), position);
-    popover.innerText = text;
+    popover.innerHTML = text;
     document.body.appendChild(popover);
 }
 
+
 function setLoadingText(word, position) {
-    setText(word, 'Loading...', position);
+    // Only show the loading text when after a small interval
+    // Because else the loading screen appears, and instantly the ajax request is done, which looks bad
+    // So wait a bit, and see if it actually has to call the KRDict (which takes long)
+    setTimeout(function () {
+        let element = getOrCreatePopOver();
+        if (element.innerHTML === '') {
+            let popover = setPopOverPosition(element, position);
+            popover.innerHTML = 'Loading...';
+            document.body.appendChild(popover);
+        }
+    }, 300);
 }
 
 function setErrorText(word, position) {
-    setText(word, 'Something went wrong fetching the translation...', position);
+    setText(word, 'Could not translate...', position);
 }
 
-function updateWordUnderCursor(word) {
-    if (wordUnderCursor !== word) {
-        hidePopOver();
-    }
 
-    wordUnderCursor = word;
+function getScrollHeight() {
+    return window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
 }
 
